6d076ef36592d25c012716de77bfd11ead6cb4c3
Olena <olenagrynyha@gmail.com>
2022-06-25 13:40:47 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
-const welcomeMessage = document.querySelector('.name');
-welcomeMessage.addEventListener('click', () => {
-     alert('Hi, my name is Olena! Welcome to my CV');
- } );
+const welcomeMessage = document.querySelector(".name");
+welcomeMessage.addEventListener("click", () => {
+  alert("Hi, my name is Olena! Welcome to my CV");
+});
 
-const colChange = document.querySelector('button');
+const colChange = document.querySelector("button");
 
 function random(number) {
-    return Math.floor(Math.random() * (number+1));
-  }
+  return Math.floor(Math.random() * (number + 1));
+}
 
-colChange.onclick = function() {
-    
-    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    const sideBarColor = document.querySelector('.sidebar');
-    sideBarColor.style.backgroundColor = rndCol;
-  };
-
-
-const changeImg = document.querySelector('img');
-changeImg.addEventListener ('click', () => {
-    changeImg.remove();
-});
+colChange.onclick = function () {
+  const rndCol =
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+  const sideBarColor = document.querySelector(".sidebar");
+  sideBarColor.style.backgroundColor = rndCol;
+};
